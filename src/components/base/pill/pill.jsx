9a097f39604c8328edaac6b4e22da7692e8b4582
Andrei U <Upolovnew@gmail.com>
2022-12-01 22:15:22 +0300

    feat: stack chips added

---
+++ src/components/base/pill/pill.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './pill.css'
+
+const Pill = ({children}) => {
+  return <span className='pill'>{children}</span>
+}
+
+export default Pill
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,74 +1,95 @@
-import React from "react";
-import "./portfolio.css";
-import PIMG01 from "../../assets/controla01.jpg";
-import PIMG02 from "../../assets/todo01.jpg";
-import PIMG03 from "../../assets/social_media.jpg";
+import React from 'react'
+import './portfolio.css'
+import PIMG01 from '../../assets/controla01.jpg'
+import PIMG02 from '../../assets/todo01.jpg'
+import PIMG03 from '../../assets/social_media.jpg'
+import Pill from '../base/pill/pill'
 
 const projects = [
   {
     id: 1,
     image: PIMG01,
-    title: "ControlApp",
-    github: "https://github.com/Anderboi/controla-notnext",
-    link: "https://anderboi.github.io/controla-notnext/#/projects",
+    stack: [
+      'HTML',
+      'CSS',
+      'JS',
+      'TS',
+      'React',
+      'Redux',
+      'Formik',
+      'MUI',
+      'Firebase',
+    ],
+    title: 'ControlApp',
+    github: 'https://github.com/Anderboi/controla-notnext',
+    link: 'https://anderboi.github.io/controla-notnext/#/projects',
     description_ru:
-      "Приложение авторского надзора. Пэт-проект, ведения журнала авторского надзора для архитекторов / дизайнеров. Делается на React, с применением Firebase (авторизация, база данных), TS, MUI, React hook forms.",
+      'Приложение авторского надзора. Пэт-проект, ведения журнала авторского надзора для архитекторов / дизайнеров. Делается на React, с применением Firebase (авторизация, база данных), TS, MUI, React hook forms.',
     description:
       "Application of author's supervision. Pet Project, maintaining a journal of author's supervision for architects / designers. It is done in React, using Firebase (authorization, database), TS, MUI, React hook forms.",
   },
   {
     id: 2,
     image: PIMG02,
-    title: "ToDoApp on js",
-    github: "https://github.com/Anderboi/mb-todo-app",
-    link: "https://anderboi.github.io/mb-todo-app/",
+    stack: [],
+    title: 'ToDoApp on js',
+    github: 'https://github.com/Anderboi/mb-todo-app',
+    link: 'https://anderboi.github.io/mb-todo-app/',
     description_ru:
-      "ToDo приложение, сделанное как тестовое задание для MindBox, по ТЗ и макету. Сделано на React, с применением typescript, classnames(npm) и добавлением небольшого количества тестов.",
+      'ToDo приложение, сделанное как тестовое задание для MindBox, по ТЗ и макету. Сделано на React, с применением typescript, classnames(npm) и добавлением небольшого количества тестов.',
     description:
-      "ToDo application, made as a test task for MindBox, on specification and layout. Made in React, using typescript, classnames(npm) and adding a small number of tests.",
+      'ToDo application, made as a test task for MindBox, on specification and layout. Made in React, using typescript, classnames(npm) and adding a small number of tests.',
   },
   {
     id: 3,
     image: PIMG03,
+    stack: [],
     title: "'Samurai' social network",
-    github: "https://github.com/Anderboi/react_social",
-    link: "https://anderboi.github.io/react_social/",
+    github: 'https://github.com/Anderboi/react_social',
+    link: 'https://anderboi.github.io/react_social/',
     description_ru:
-      "Учебный проект по React. Небольшая социальная сеть. Функционал не весь, но больше чем в самом курсе.Сам дописывал логику добавления друзей, обмена сообщениями.",
+      'Учебный проект по React. Небольшая социальная сеть. Функционал не весь, но больше чем в самом курсе.Сам дописывал логику добавления друзей, обмена сообщениями.',
     description:
-      "React training project. A small social network. The functionality is not all, but more than in the course itself. I implemented the logic of adding friends and messaging.",
+      'React training project. A small social network. The functionality is not all, but more than in the course itself. I implemented the logic of adding friends and messaging.',
   },
-];
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio" className="section">
-      <h5 className="sub-subheader">My Recent Projects</h5>
-      <h2 className="subheader">Portfolio</h2>
+    <section id='portfolio' className='section'>
+      <h5 className='sub-subheader'>My Recent Projects</h5>
+      <h2 className='subheader'>Portfolio</h2>
 
-      <div className="container portfolio">
-        {projects.map(({ id, title, image, description, github, link }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item__image">
-                <img src={image} alt={`portfolio ${id}`} />
-              </div>
-              <h3 className="portfolio__item__header">{title}</h3>
-              <p className="portfolio__item__description">{description}</p>
-              <div className="portfolio__item__button-block">
-                <a href={github} className="btn" target="_blanc">
-                  Github
-                </a>
-                <a href={link} className="btn btn-primary" target="_blanc">
-                  Live Demo
-                </a>
-              </div>
-            </article>
-          );
-        })}
+      <div className='container portfolio'>
+        {projects.map(
+          ({id, title, image, description, github, link, stack}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item__image'>
+                  <img src={image} alt={`portfolio ${id}`} />
+                </div>
+                <div className='portfolio__item__chips'>
+                  {stack.map((tech, idx) => {
+                    return <Pill key={idx}>{tech}</Pill>
+                  })}
+                </div>
+                <h3 className='portfolio__item__header'>{title}</h3>
+                <p className='portfolio__item__description'>{description}</p>
+                <div className='portfolio__item__button-block'>
+                  <a href={github} className='btn' target='_blanc'>
+                    Github
+                  </a>
+                  <a href={link} className='btn btn-primary' target='_blanc'>
+                    Live Demo
+                  </a>
+                </div>
+              </article>
+            )
+          }
+        )}
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Portfolio;
+export default Portfolio
